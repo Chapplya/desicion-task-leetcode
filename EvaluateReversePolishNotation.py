918bdc71cf98e1e4bca2644b31ef04105c53e05c
Denis <den12262@mail.ru>
2025-03-07 13:58:15 +0300

    stack-3: second solution

--- EvaluateReversePolishNotation.py
+++ EvaluateReversePolishNotation.py
@@ -1,28 +1,28 @@
 class Solution:
     def evalRPN(self, tokens: list[str]) -> int:
         stack = []
-        elem_ = {'+', '-', '*', '/'}
-        if tokens[0] in elem_: return False
+        elem_ = {"+", "-", "*", "/"}
+        if tokens[0] in elem_:
+            return False
         for i in range(len(tokens)):
             if tokens[i] not in elem_:
                 stack.append(int(tokens[i]))
             else:
-                if tokens[i] == '+':
-                    stack.append(stack[-2]+stack[-1])
-                elif tokens[i] == '-':
-                    stack.append(stack[-2]-stack[-1])
-                elif tokens[i] == '*':
-                    stack.append(stack[-2]*stack[-1])
-                elif tokens[i] == '/':
-                    stack.append(int(stack[-2]/stack[-1]))
-        
-        return stack[-1]
+                if tokens[i] == "+":
+                    stack.append(stack.pop() + stack.pop())
+                elif tokens[i] == "-":
+                    a, b = stack.pop(), stack.pop()
+                    stack.append(b - a)
+                elif tokens[i] == "*":
+                    stack.append(stack.pop() * stack.pop())
+                elif tokens[i] == "/":
+                    a, b = stack.pop(), stack.pop()
+                    stack.append(int(float(b) / a))
+        return stack[0]
+
 
 settings = Solution()
 
-tokens = ["1","2","+","3","*","4","-"]
+tokens = ["1", "2", "+", "3", "*", "4", "-"]
 
 print(settings.evalRPN(tokens))
-                
-                
-                
