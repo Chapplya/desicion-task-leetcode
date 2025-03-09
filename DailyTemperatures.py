0d3e7fe2ca24b9a7e431684f6886adc0fc7930b6
Denis <den12262@mail.ru>
2025-03-09 14:07:25 +0300

    stack-5: time O(n) space O(n)

--- DailyTemperatures.py
+++ DailyTemperatures.py
@@ -1,18 +1,17 @@
 class Solution:
     def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
-        res = []
-        for i in range(len(temperatures)):
-            for j in range(i, len(temperatures)):
-                if temperatures[i] < temperatures[j]:
-                    res.append(j-i)
-                    break
-                if j == len(temperatures)-1:
-                    res.append(0)
+        res = [0] * len(temperatures)
+        stack = []  
 
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = i - stackInd
+            stack.append((t, i))
         return res
         
 settings = Solution()
 
-temperatures = [30,38,30,36,35,40,28]   
+temperatures = [30,38,30,36,35,40,28]  
 
 print(settings.dailyTemperatures(temperatures))
