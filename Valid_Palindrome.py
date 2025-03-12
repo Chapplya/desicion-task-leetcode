c5cfe97931e3a74e7bd782a41206eb63a775d130
Denis <den12262@mail.ru>
2025-03-12 10:57:40 +0300

    pointers-1: more eazy solution

--- Valid_Palindrome.py
+++ Valid_Palindrome.py
@@ -2,17 +2,9 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         s_alph = ''
         for elem in s:
-            if 97 <= ord(elem) <= 122 or 48 <= ord(elem) <=57 or 65 <= ord(elem) <= 90: 
-                s_alph += elem
-        s_alph = s_alph.lower()
-        print(s_alph)
-        j = len(s_alph)-1
-        
-        for i in range(len(s_alph)//2):
-            if s_alph[i] != s_alph[j-i]:
-                return False
-        return True
-            
+            if elem.isalnum():
+                s_alph += elem.lower()
+        return s_alph == s_alph[::-1]
  
 settings = Solution()       
 s = "A man, a plan, a canal: Panama"
