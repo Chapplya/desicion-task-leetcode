a89049d682148e5f2d8f68f41bdf442b9ec964d1
Denis <den12262@mail.ru>
2025-03-12 14:19:35 +0300

    pointers-2: fast solution

--- TwoIntegerSumII.py
+++ TwoIntegerSumII.py
@@ -1,12 +1,17 @@
 class Solution:
     def twoSum(self, numbers: list[int], target: int) -> list[int]:
-        
-        for i in range(len(numbers)):
-            for j in range(i+1, len(numbers)):
-                if numbers[i] + numbers[j] == target and numbers[i] != numbers[j]:
-                    return [i+1,j+1]
-        return []
 
+        left = 0
+        right = len(numbers)-1
+        while left < right:
+            summ = numbers[left] + numbers[right]
+            if summ > target:
+                right -= 1
+            elif summ < target:
+                left += 1
+            else:
+                return [left + 1, right + 1]
+        return []
 
 
 settings = Solution()
