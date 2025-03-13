0e03a1fc39c300fd30422c0022e601d2343a1578
Denis <den12262@mail.ru>
2025-03-13 19:14:54 +0300

    pointers: ruff

--- Valid_Palindrome.py
+++ Valid_Palindrome.py
@@ -1,21 +1,26 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s) - 1
+        left, right = 0, len(s) - 1
 
-        while l < r:
-            while l < r and not self.alphaNum(s[l]):
-                l += 1
-            while r > l and not self.alphaNum(s[r]):
-                r -= 1
-            if s[l].lower() != s[r].lower():
+        while left < right:
+            while left < right and not self.alphaNum(s[left]):
+                left += 1
+            while right > left and not self.alphaNum(s[right]):
+                right -= 1
+            if s[left].lower() != s[right].lower():
                 return False
+            left += 1  
+            right -= 1 
 
-        def alphaNum(self, c):
-            return (
-                ord("A") <= ord(c) <= ord("Z")
-                or ord("a") <= ord(c) <= ord("z")
-                or ord("0") <= ord(c) <= ord("9")
-            )
+        return True 
+
+    def alphaNum(self, c):
+        return (
+            ord("A") <= ord(c) <= ord("Z")
+            or ord("a") <= ord(c) <= ord("z")
+            or ord("0") <= ord(c) <= ord("9")
+        )
+
 
 
 settings = Solution()
