ca0e62ca4f7f110fc9aab462e9e287b6250fa990
Denis <den12262@mail.ru>
2025-03-13 16:34:33 +0300

    pointers-1: solution with two pointers

--- Valid_Palindrome.py
+++ Valid_Palindrome.py
@@ -1,11 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s_alph = ''
-        for elem in s:
-            if elem.isalnum():
-                s_alph += elem.lower()
-        return s_alph == s_alph[::-1]
- 
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+
+        def alphaNum(self, c):
+            return (ord('A') <= ord(c) <= ord('Z') or 
+                    ord('a') <= ord(c) <= ord('z') or 
+                    ord('0') <= ord(c) <= ord('9'))
 settings = Solution()       
 s = "A man, a plan, a canal: Panama"
 
