7557569aea25be070519a9fcd8b36bd02adcd47d
Denis <den12262@mail.ru>
2025-03-18 22:52:04 +0300

    binary-2: fast solution

--- Search a 2D Matrix.py
+++ Search a 2D Matrix.py
@@ -1,37 +1,31 @@
 class Solution:
     def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
-        if not matrix or not matrix[0]: 
+        if not matrix or not matrix[0]:
             return False
-        i = 0
-        while i <= len(matrix)-1:
-            if matrix[i][0] <= target <= matrix[i][-1]:
-                if target == matrix[i][0] or target == matrix[i][-1]:
-                    return True
-                else:
-                    left = 0
-                    right = len(matrix[0])-1
-                    while left <= right:
-                        mid = (left + right)//2
-                        if matrix[i][mid] == target:
-                            return True
-                        elif matrix[i][mid] < target:
-                            left = mid + 1
-                        else:
-                            right = mid - 1
-                    return False
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        left, right = 0, m * n - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            mid_value = matrix[mid // n][mid % n]
+
+            if mid_value == target:
+                return True
+            elif mid_value < target:
+                left = mid + 1
             else:
-                i += 1
-        return  False
+                right = mid - 1
+
+        return False
+
 
 sol = Solution()
 
-matrix = [[-8,-7,-5,-3,-3,-1,1],[2,2,2,3,3,5,7],[8,9,11,11,13,15,17],[18,18,18,20,20,20,21],[23,24,26,26,26,27,27],[28,29,29,30,32,32,34]]
+matrix = [[1, 2, 3, 4], [5, 6, 7, 8]]
 
-target = -5
+target = 7
 
-print (sol.searchMatrix(matrix, target))
-
-
-
-                        
-            
+print(sol.searchMatrix(matrix, target))
