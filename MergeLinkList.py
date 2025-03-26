d11b875077529872eadcb1ff781029e0bad4e85e
Denis <den12262@mail.ru>
2025-03-26 17:41:15 +0300

    lklist-4: first super sol

--- MergeLinkList.py
+++ MergeLinkList.py
@@ -1,25 +1,36 @@
-
 class Solution:
-    def __init__(self, root1, root2, res):
-        self.root1 = root1
-        self.root2 = root2
-        self.res = res
+    def reorderList(self, head) -> None:
+        if not head or not head.next:
+            return
+ 
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
         
-    def mergeTwoLists(self):
-        lst1 = self.root1
-        lst2 = self.root2
-        start_res = self.res
-        new_lst = start_res
 
-        while lst1 and lst2:
-            if lst1.val > lst2.val:
-                new_lst.next = lst2
-                lst2 = lst2.next
-            else:
-                new_lst.next = lst1
-                lst1 = lst1.next
-            new_lst = new_lst.next
-        new_lst.next = lst1 or lst2
+        second_half = slow.next
+        slow.next = None 
+        prev = None
+        
 
-        self.res = start_res.next
+        while second_half:
+            nxt = second_half.next
+            second_half.next = prev
+            prev = second_half
+            second_half = nxt
+        
+
+        first_half = head
+        second_half = prev
+        
+        while second_half:
+            temp1 = first_half.next
+            temp2 = second_half.next
+            
+            first_half.next = second_half
+            second_half.next = temp1
+            
+            first_half = temp1
+            second_half = temp2
 
