e1cde2e1a4dec230d140a03c8fb0b32d960ddbb2
Denis <den12262@mail.ru>
2025-03-29 12:45:28 +0300

    lklist-6: small problem

--- CopyLinkedistithRandomPointer.py
+++ CopyLinkedistithRandomPointer.py
@@ -1,15 +1,12 @@
-"""
-# Definition for a Node.
 class Node:
-    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+    def __init__(self, x: int, next: "Node" = None, random: "Node" = None):
         self.val = int(x)
         self.next = next
         self.random = random
-"""
 
 
 class Solution:
-    def copyRandomList(self, head: "Optional[Node]") -> "Optional[Node]":
+    def copyRandomList(self, head):
         if not head:
             return None
 
