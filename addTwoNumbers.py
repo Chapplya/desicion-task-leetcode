2e43c67f77347abd3ad18cc7a879a81049d2eb87
Denis <den12262@mail.ru>
2025-03-29 20:27:12 +0300

    lklist-7: black

--- addTwoNumbers.py
+++ addTwoNumbers.py
@@ -2,7 +2,8 @@ class Node:
     def __init__(self, val=None, next=None):
         self.val = None
         self.next = None
-        
+
+
 class Solution:
     def addTwoNumbers(self, l1, l2):
         result = Node()
@@ -11,12 +12,12 @@ class Solution:
         while l1 or l2 or ost:
             val1 = l1.val if l1 else 0
             val2 = l2.val if l2 else 0
-            
+
             value = val1 + val2 + ost
             ost = value // 10
             val = value % 10
             tmp.next = Node(val)
-            
+
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
         return result.next
